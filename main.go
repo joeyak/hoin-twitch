ac37b13cb825bf7a326082b7fdb4276998efac07
joeyak <joeyak97@gmail.com>
2022-04-21 14:27:39 -0400

    end process if out of paper

--- main.go
+++ main.go
@@ -14,47 +14,47 @@ import (
 )
 
 var args struct {
-	SkipPrinter bool     `arg:"--skip"`
-	Channels    []string `arg:"-c,required"`
-	User        string   `arg:"-u,required"`
-	Token       string   `arg:"-t,required"`
-	Addr        string   `default:"192.168.1.23:9100"`
+	Channels []string `arg:"-c,required"`
+	User     string   `arg:"-u,required"`
+	Token    string   `arg:"-t,required"`
+	Addr     string   `default:"192.168.1.23:9100"`
 }
 
 func main() {
 	arg.MustParse(&args)
 
-	if !args.SkipPrinter {
-		conn, err := net.Dial("tcp", args.Addr)
-		if err != nil {
-			fmt.Println("unable to dial:", err)
-			return
-		}
-		defer conn.Close()
-
-		printer := hoin.NewPrinter(conn)
-		defer func() {
-			printer.Println("Closing client")
-			printer.CutFeed(100)
-		}()
-
-		printer.Printf("Joining %s\n", args.Channels)
-
-		log.SetOutput(io.MultiWriter(os.Stdout, printer))
+	conn, err := net.Dial("tcp", args.Addr)
+	if err != nil {
+		fmt.Println("unable to dial:", err)
+		return
 	}
+	defer conn.Close()
+
+	printer := hoin.NewPrinter(conn)
+	defer func() {
+		printer.Println("Closing client")
+		printer.CutFeed(100)
+	}()
+
+	printer.Initialize()
+
+	printer.Printf("Joining %s\n", args.Channels)
+
+	log.SetOutput(io.MultiWriter(os.Stdout, printer))
 
 	log.SetPrefix("\n  ")
 	log.SetFlags(log.Lmsgprefix | log.LstdFlags)
 
-	go connectClient()
-
 	fmt.Println("Press ctrl+c to shutdown")
 	ch := make(chan os.Signal, 5)
 	signal.Notify(ch, os.Interrupt)
+
+	go connectClient(printer, ch)
+
 	<-ch
 }
 
-func connectClient() {
+func connectClient(printer hoin.Printer, ch chan os.Signal) {
 	client := twitch.NewClient(args.User, args.Token)
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
@@ -67,6 +67,15 @@ func connectClient() {
 			log.Printf("[%s] %s: %s\n", message.Channel, message.User.DisplayName, message.Message)
 		} else {
 			log.Printf("%s: %s\n", message.User.DisplayName, message.Message)
+		}
+
+		status, err := printer.TransmitPaperSensorStatus()
+		if err != nil {
+			log.Fatalf("Could not get paper sensor status: %v\n", err)
+		}
+		if status.NearEnd || status.RollEnd {
+			log.Println("Out of paper")
+			ch <- os.Kill
 		}
 	})
 
